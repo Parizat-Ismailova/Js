c77afef2df49b9f1d452528b45a329ef8f578710
Parizat Ismailova <parizat.ismailova.kk@gmail.com>
2021-09-21 12:37:04 -0500

    My first script in JS

---
+++ script.js
@@ -0,0 +1,7 @@
+alert('Я начинаю изучать JS');
+console.log('Мое первое сообщение в консоли');
+document.write('<h1>Привет!</h1> <p><b>Это мой первый скрипт!</b></p>');
+
+console.log("Peri" + "Yan");
+console.log(100 + 100);
+document.write("100 + 100 = " + (100 + 100));
